7f58653b4160ca938b176b5c6974eb7150c1e462
Priyanshu Gupta <priyanshugupat379@gmail.com>
2026-08-22 19:21:04 +0530

    Use env variable for API URL

--- ecommerce-frontend/src/services/api.js
+++ ecommerce-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Laravel backend address - yahi se sara data aayega
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 // agar user login h, to har request ke sath uska token bhi bhej denge
